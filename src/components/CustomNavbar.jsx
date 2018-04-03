b27949a68e0822822562e7f733b88098324a8b84
lallhar <lfc5timesktt@gmail.com>
2018-04-03 16:36:15 +0530

    First commit

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -1,32 +1,29 @@
 import React, { Component } from 'react'
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Navbar, FormGroup, FormControl, Button } from 'react-bootstrap';
+
 import './CustomNavbar.css';
 
 export default class CustomNavbar extends Component {
   render() {
     return (
-      <Navbar default collapseOnSelect>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <Link to="/">My First React Project</Link>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav pullRight>
-            <NavItem eventKey={1} componentClass={Link} href="/" to="/">
-              Home
-            </NavItem>
-            <NavItem eventKey={2} componentClass={Link} href="/about" to="/about">
-              About
-            </NavItem>
-            <NavItem eventKey={3} componentClass={Link} href="/news" to="/news">
-              News
-            </NavItem>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <Navbar>
+      <navbar-inverse>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="#home">Squarespace</a>
+      </Navbar.Brand>
+
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Navbar.Form pullLeft>
+        <FormGroup className="place">
+          <FormControl type="text" placeholder="SEARCH FOR A DOMAIN" />
+        </FormGroup>{' '}
+        <Button type="submit">GET STARTED</Button>
+      </Navbar.Form>
+    </Navbar.Collapse>
+    </navbar-inverse>
+  </Navbar>
     )
   }
 }
